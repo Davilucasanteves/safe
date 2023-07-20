ec5c7e5369ed96b90c8c58029461a3fc8477fa76
Daví Lucas <127633524+Davilucasanteves@users.noreply.github.com>
2023-07-20 10:11:21 -0300

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -1,1 +1,9 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 System.ou.println("oi");
+
+//public, protected, default, private; throws ContaExcecao(classe da excecao)--> extends Execption
+//visibilidade em java
+//Caso seja uma subclasse de Exeption ela é verificada, já se for uma subclasse de RunTimeException ou Error será não verificada.
+//Exeção Verificada são forçadas pelo próprio compilador(ClassNotFound); já as Não Verificadas ocorrem durante a execução do programa(dividir por zero) 
