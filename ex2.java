1be59966b8db8a66d60c2d2922e15830936c6407
Daví Lucas <127633524+Davilucasanteves@users.noreply.github.com>
2023-10-31 16:55:47 -0300

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -7,3 +7,17 @@ System.ou.println("oi");
 //visibilidade em java
 //Caso seja uma subclasse de Exeption ela é verificada, já se for uma subclasse de RunTimeException ou Error será não verificada.
 //Exeção Verificada são forçadas pelo próprio compilador(ClassNotFound); já as Não Verificadas ocorrem durante a execução do programa(dividir por zero) 
+public class Main
+{
+	public static void main(String[] args) {
+	    Fracao f= new Fracao(8,2);
+	    Fracao f2= new Fracao();
+	    
+	    
+	    f2.setNumerador(5);
+	    f2.setDenominador(0);
+	    
+	    f.exibeResultado();
+	    f2.exibeResultado();
+	}
+}
